e48ddc5e3f6296927ce2e4aeaa1320eab09b105e
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-12-10 11:04:36 +0100

    0.5.4 automatically generated release

--- genex/__init__.py
+++ genex/__init__.py
@@ -16,6 +16,6 @@ from genex.example import extract
 from genex.example import generate
 from genex.example import todolist
 
-__version__ = '0.5.3'
+__version__ = '0.5.4'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
