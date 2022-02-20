b792d34de185c3b2c26071d59e397a5d4e2244f4
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-02-20 02:42:13 +0100

    test(pages): verify title page selection

---
+++ tests/test_pages.py
@@ -0,0 +1,37 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021-2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import os
+
+import iamraw
+import power
+import utila
+import utilatest
+
+import genex
+
+
+@utilatest.longrun
+def test_select_titlepage(testdir):
+    files = [
+        power.BACHELOR090_PDF,
+    ]
+    genex.extract(
+        files,
+        destination=testdir.tmpdir,
+        groupme=True,
+        sections=True,
+        detector=False,
+        base=power.REPOSITORY,
+        pages='0:10',
+    )
+    generated = os.path.join(testdir.tmpdir, 'bachelor_bachelor090')
+    utila.exists_assert(generated)
+    selected = genex.select_pages(generated, select=iamraw.TitlePageSection)
+    assert selected == '1'
